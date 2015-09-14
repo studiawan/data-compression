2f6ce47df4c53836d6b9c0d96ba3ea920da6ebbe
Hudan Studiawan <studiawan@gmail.com>
2015-09-14 10:48:54 +0700

    add directory for improved Arithmetic coding

---
+++ arithmetic_n/arith_n.c
@@ -0,0 +1,16 @@
+/*
+* * This is the order-n arithmetic coding module used in the final
+* part of chapter 6.
+*
+* Compile with BITIO.C. ERRHAND.C, and either MAIN-C.C OR MAIN-E.C. This
+* program should be compiled in large model. An even better alternative
+* is a DOS extender.
+*
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "errhand.h"
+#include "bitio.h"
+
