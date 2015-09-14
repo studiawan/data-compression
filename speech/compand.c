9984eeea1335066e2d8276bff1e7eb7cfea17d16
Hudan Studiawan <studiawan@gmail.com>
2015-09-14 11:59:53 +0700

    add directory for speech compression

---
+++ speech/compand.c
@@ -0,0 +1,14 @@
+/************************ Start of COMPAND.C ************************
+*
+* This is the companding module used in chapter 10.
+* Compile with BITIO.C, ERRHAND.C, and either MAIN-C.C or MAIN-E.C
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <math.h>
+#include "bitio.h"
+#include "errhand.h"
+#include "main.h"
---
+++ speech/silence.c
@@ -0,0 +1,15 @@
+/************************ Start of SILENCE.C ************************
+*
+* This is the silence compression coding module used in chapter 10.
+* Compile with BITIO.C, ERRHAND.C, and either MAIN-C.C or MAIN-E.C
+* 
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include "bitio.h"
+#include "errhand.h"
+#include "main.h"
+
