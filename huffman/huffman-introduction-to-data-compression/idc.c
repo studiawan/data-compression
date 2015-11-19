48fc3fd5c8d1b52122eeb650cc983ffc14f63571
Miftahuddin Al Anshory <miftah.anshory12@gmail.com>
2015-11-19 12:36:28 +0700

    tidy up huff_dec.c and idc.c

--- huffman/huffman-introduction-to-data-compression/idc.c
+++ huffman/huffman-introduction-to-data-compression/idc.c
@@ -1,45 +1,57 @@
 #include "idc.h"
 
-void value(int *values, unsigned char *image, int size,int num){
+void value(int *values, unsigned char *image, int size,int num)
+{
 	int i;
-	for(i=0;i<size;i++){
+	for(i = 0; i < size; i++)
+    {
 		values[image[i]]++;
 	}
 	return;
 }
 
-void getcode(FILE *fp, int num, unsigned int *code, char *length){
+void getcode(FILE *fp, int num, unsigned int *code, char *length)
+{
 	return;
 }
 
-void sort(float *prob, int *loc,int num){
+void sort(float *prob, int *loc,int num)
+{
 	int i,j;
 	//selection sort descending
-	for(i=0;i<num;i++){
-		loc[i]=i;
+	for(i = 0; i < num; i++)
+    {
+		loc[i] = i;
 	}
-	for(i=0;i<num-1;i++){
-		int temp=i;
-		for(j=i+1;j<num;j++){
-			if(prob[loc[temp]]<prob[loc[j]]){
-				temp=j;
+
+	for(i = 0; i < num-1; i++)
+    {
+		int temp = i;
+		for(j = i+1; j < num; j++)
+		{
+			if(prob[loc[temp]] < prob[loc[j]])
+			{
+				temp = j;
 			}
-			else if(prob[loc[temp]]==prob[loc[j]] && loc[j]<loc[temp]){
-				temp=j;
+			else if(prob[loc[temp]] == prob[loc[j]] && loc[j] < loc[temp])
+			{
+				temp = j;
 			}
 		}
 		int tmp = loc[i];
-		loc[i]=loc[temp];
-		loc[temp]=tmp;
+		loc[i] = loc[temp];
+		loc[temp] = tmp;
 	}
 	return;
 }
 
-void huff(float prob[], int loc[], int num, unsigned int *code, char *length){
+void huff(float prob[], int loc[], int num, unsigned int *code, char *length)
+{
 	return;
 }
 
-int files(int size,int *code,char *length,unsigned char *file){
+int files(int size,int *code,char *length,unsigned char *file)
+{
 	return;
 }
 
