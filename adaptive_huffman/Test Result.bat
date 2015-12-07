777e193d54e6db30b1fd59424b4d391e7f05de1d
irfanrahmat007 <irfanrahmat007@hotmail.com>
2015-12-07 14:11:12 +0700

    Using file from lib folder to compile
    Update Readme : Instruction for compiling using source from lib folder
    
    Add batch file for windows

---
+++ adaptive_huffman/Test Result.bat
@@ -0,0 +1,45 @@
+@echo OFF
+main-c.exe ..\cantrbry\alice29.txt ahuff.ahuff -d
+main-e.exe ahuff.ahuff ..\result\adaptive_huffman\hasil_kompresi\alice29.txt
+notepad ..\result\adaptive_huffman\hasil_kompresi\alice29.txt
+PAUSE
+main-c.exe ..\cantrbry\asyoulik.txt ahuff.ahuff -d
+main-e.exe ahuff.ahuff ..\result\adaptive_huffman\hasil_kompresi\asyoulik.txt
+notepad ..\result\adaptive_huffman\hasil_kompresi\asyoulik.txt
+PAUSE
+main-c.exe ..\cantrbry\cp.html ahuff.ahuff -d
+main-e.exe ahuff.ahuff ..\result\adaptive_huffman\hasil_kompresi\cp.html
+notepad ..\result\adaptive_huffman\hasil_kompresi\cp.html
+PAUSE
+main-c.exe ..\cantrbry\fields.c ahuff.ahuff -d
+main-e.exe ahuff.ahuff ..\result\adaptive_huffman\hasil_kompresi\fields.c
+notepad ..\result\adaptive_huffman\hasil_kompresi\fields.c
+PAUSE
+main-c.exe ..\cantrbry\grammar.lsp ahuff.ahuff -d
+main-e.exe ahuff.ahuff ..\result\adaptive_huffman\hasil_kompresi\grammar.lsp
+notepad ..\result\adaptive_huffman\hasil_kompresi\grammar.lsp
+PAUSE
+main-c.exe ..\cantrbry\kennedy.xls ahuff.ahuff -d
+main-e.exe ahuff.ahuff ..\result\adaptive_huffman\hasil_kompresi\kennedy.xls
+..\result\adaptive_huffman\hasil_kompresi\kennedy.xls
+PAUSE
+main-c.exe ..\cantrbry\lcet.txt ahuff.ahuff -d
+main-e.exe ahuff.ahuff ..\result\adaptive_huffman\hasil_kompresi\lcet.txt
+notepad ..\result\adaptive_huffman\hasil_kompresi\lcet.txt
+PAUSE
+main-c.exe ..\cantrbry\plrabn12.txt ahuff.ahuff -d
+main-e.exe ahuff.ahuff ..\result\adaptive_huffman\hasil_kompresi\plrabn12.txt
+notepad ..\result\adaptive_huffman\hasil_kompresi\plrabn12.txt
+PAUSE
+main-c.exe ..\cantrbry\ptt5 ahuff.ahuff -d
+main-e.exe ahuff.ahuff ..\result\adaptive_huffman\hasil_kompresi\ptt5
+notepad ..\result\adaptive_huffman\hasil_kompresi\ptt5
+PAUSE
+main-c.exe ..\cantrbry\sum ahuff.ahuff -d
+main-e.exe ahuff.ahuff ..\result\adaptive_huffman\hasil_kompresi\sum
+notepad ..\result\adaptive_huffman\hasil_kompresi\sum
+PAUSE
+main-c.exe ..\cantrbry\xargs.1 ahuff.ahuff -d
+main-e.exe ahuff.ahuff ..\result\adaptive_huffman\hasil_kompresi\xargs.1
+notepad ..\result\adaptive_huffman\hasil_kompresi\xargs.1
+PAUSE
